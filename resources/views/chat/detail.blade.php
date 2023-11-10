8f7e15fc16b9a3b32c716de9a02e030bd856eb1b
zhanghang <zhanghang@blued.com>
2023-11-10 18:14:52 +0800

    feat: 添加路由

--- resources/views/chat/detail.blade.php
+++ resources/views/chat/detail.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
-    <title>6体育生</title>
+    <title>{{env('APP_NAME')}}</title>
     <link rel="stylesheet" href="/chat/chat_detail.css">
     <script src="/chat/doubleclick_scroll.js"></script>
 </head>
