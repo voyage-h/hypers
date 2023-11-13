053f367ba54ac2ff6d0f2633fcf57fde6da323ca
moka <voyage1314@gmail.com>
2023-11-13 23:29:10 +0800

    feat: 231113

--- public/chat/detail.js
+++ public/chat/detail.js
@@ -1,23 +1,76 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // 警告
-    const warning = document.getElementById('alertWarning');
+// 获取所有图片元素
+const contentImgs = document.querySelectorAll(".contents-img");
+let currentImg = null;
+let isTouchMoved = false; // 新增变量，标记是否发生了滑动
 
-    // 点击图片放大
-    const contentImg = document.querySelector(".contents-img");
-    contentImg.addEventListener('click', function () {
+// 遍历每个图片元素并添加触摸事件
+contentImgs.forEach(function (contentImg) {
+    contentImg.addEventListener('touchstart', function (event) {
+        isTouchMoved = false; // 重置滑动标记
+        if (currentImg) {
+            currentImg.img.remove();
+            currentImg.overlay.remove();
+        }
+
+        // 创建背景遮罩
+        const overlay = document.createElement('div');
+        overlay.style.position = 'fixed';
+        overlay.style.top = '0';
+        overlay.style.left = '0';
+        overlay.style.width = '100%';
+        overlay.style.height = '100%';
+        overlay.style.backgroundColor = 'black'; // 半透明黑色
+        overlay.style.zIndex = '9998';
+
+        // 创建新的图片元素
         const img = document.createElement('img');
-        img.src = contentImg.getAttribute('data-src');
-        img.style.width = '100%';
-        img.style.height = 'auto';
+        img.src = contentImg.getAttribute('data-src') + '!o.png';
         img.style.position = 'fixed';
-        img.style.top = '0';
-        img.style.left = '0';
+        img.style.top = '50%';
+        img.style.left = '50%';
+        img.style.transform = 'translate(-50%, -50%)'; // 居中
         img.style.zIndex = '9999';
         img.style.cursor = 'zoom-out';
-        img.addEventListener('click', function () {
-            img.remove();
+        img.style.maxWidth = '100%';
+        img.style.maxHeight = '100%';
+
+        // 添加触摸事件，关闭放大的图片
+        overlay.addEventListener('touchend', function () {
+            if (!isTouchMoved) { // 仅当未发生滑动时才关闭
+                img.remove();
+                overlay.remove();
+                currentImg = null;
+            }
         });
+        img.addEventListener('touchend', function () {
+            if (!isTouchMoved) {
+                img.remove();
+                overlay.remove();
+                currentImg = null;
+            }
+        });
+		
+
+        // 将新的图片元素和背景遮罩添加到 body 中
+        document.body.appendChild(overlay);
         document.body.appendChild(img);
 
+        currentImg = { img, overlay };
     });
+
+    contentImg.addEventListener('touchmove', function () {
+        isTouchMoved = true; // 标记发生了滑动
+    });
+});
+
+// 触摸空白处关闭图片
+document.addEventListener('touchstart', function (event) {
+    if (event.target.tagName.toLowerCase() === 'body' && currentImg) {
+        currentImg.img.remove();
+        currentImg.overlay.remove();
+        currentImg = null;
+    }
+});
+
 });
--- resources/views/chat/detail.blade.php
+++ resources/views/chat/detail.blade.php
@@ -8,29 +8,6 @@
     <script src="/chat/detail.js"></script>
 </head>
 <body>
-<!-- 弹窗容器 -->
-<div class="modal" id="myModal">
-    <div class="modal-content">
-        <span class="close">&times;</span>
-        <h2>用户信息</h2>
-        <form id="userForm" enctype="multipart/form-data">
-            <input type="hidden" id="target" name="target" value="">
-            <label for="name">修改备注:</label>
-            <input type="text" id="note" name="note"><br><br>
-            <label for="avatar">真实头像:</label>
-            <input type="file" id="real_avatar" name="real_avatar" accept="image/*"><br><br>
-            <label for="remove_real_avatar">是否删除真实头像:</label>
-            <input type="radio" id="remove_real_avatar" name="remove_real_avatar" value=1>
-            <label for="remove_real_avatar_yes">是</label>
-            <input type="radio" id="remove_real_avatar_no" name="remove_real_avatar" value=0 checked>
-            <label for="remove_real_avatar_no">否</label>
-            <div class="submit-container">
-                <button type="submit">提交</button>
-            </div>
-        </form>
-    </div>
-</div>
-
 <div class="chat-container">
     @switch(request()->input('show'))
         @case('follow')
