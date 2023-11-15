7471970d87e381de69c13989d0331b729ad2e7eb
moka <voyage1314@gmail.com>
2023-11-15 16:52:18 +0800

    feat: 修改没有数据时提示

--- resources/views/chat/detail/time.blade.php
+++ resources/views/chat/detail/time.blade.php
@@ -5,10 +5,10 @@
     <!-- 如果是昨天，展示昨天和小时 -->
     昨天 {{$time->format('H:i')}}
 @elseif ($time->isCurrentYear())
-    <!-- 如果是今年，展示月日和小时 -->
-    {{$time->format('m-d H:i')}}
+        {{$time->format('m-d H:i')}}
 @else
-    <!-- 如果是去年，展示年月日和小时 -->
-    {{$time}}
+    <!-- 如果是去年，展示年月日 -->
+    {{$time->format('Y-m-d')}}
 @endif
+
 
