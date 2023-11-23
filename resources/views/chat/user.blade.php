e5dd011038bcab73a0adc08e88f5429d1dd03976
moka <voyage1314@gmail.com>
2023-11-23 18:20:57 +0800

    feat: 231123

--- resources/views/chat/user.blade.php
+++ resources/views/chat/user.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
     <title>{{$me->name ?? env('APP_NAME')}}</title>
-    <link rel="stylesheet" href="{{url('/chat/user.css')}}">
+    <link rel="stylesheet" href="{{url('/chat/user1.css')}}">
+    <link rel="stylesheet" href="/photoswipe/photoswipe.css">	
     <script src="{{url('/chat/user.js')}}"></script>
 </head>
 <body>
@@ -70,14 +71,18 @@
                 <div class="menu-title chat-albums-title">我的</div>
                 <div class="menu-right chat-albums-right"><img src="/chat/right.png"></div>
             </div>
+        </a>
             @if(! empty($me->albums[0]))
             <div class="menu-items">
                 @foreach($me->albums as $album)
-                    <div class="menu-item"><img src="{{$album->contents}}"></div>
+                    <div class="menu-item">
+					    <a href="{{$album->contents}}" class="contents-img-a" data-pswp-src="{{$album->contents}}!o.png">
+						    <img src="{{$album->contents}}">
+						</a>
+					</div>
                 @endforeach
             </div>
             @endif
-        </a>
 		<div class="seperate"></div>
         <a class="menu-a" href="/chat/{{$me->uid}}/all">
         <div class="menu menu-active">
@@ -124,4 +129,5 @@
 <div class="page" id="page">{{empty($start) ? '' : ' -- 没有更多 -- '}}</div>
 @include('chat.detail.modal', ['uid' => $me->uid])
 </body>
+@include('components.photoswipe', ['gallery' => '.menu-items', 'children' => '.contents-img-a'])
 </html>
